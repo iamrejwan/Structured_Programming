235bf2539094567843a912b6af4b00c9b475bd49
coder9923 <55855042+coder9923@users.noreply.github.com>
2022-05-20 20:00:11 +0600

    solved

---
+++ Class-1/exercise_5.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+	double length,breadth,radius,area_rectangle,perimeter_rectangle,area_circle,circumference_circle;
+	
+	printf("Enter the length:");
+	scanf("%lf",&length);
+	printf("Enter the breadth:");
+	scanf("%lf",&breadth);
+	printf("Enter the radius:");
+	scanf("%lf",&radius);
+	
+	
+	area_rectangle=length*breadth;
+	perimeter_rectangle=2*(length+breadth);
+	area_circle=3.1416*radius*radius;
+	circumference_circle=2*3.1416*radius;
+	
+	printf("Area of rectangle:%.2lf\n",area_rectangle);
+	printf("Perimeter of rectangle:%.2lf\n",perimeter_rectangle);
+	printf("Area of circle:%.2lf\n",area_circle);
+	printf("Perimeter of circle:%.2lf",	circumference_circle);
+	
+	
+	
+	return 0;
+	
+}
